109f3d0ebc7e569640cc7fc500960c600cb841ea
David Hale <dhale12@hotmail.co.uk>
2017-05-04 14:13:41 +0100

    initial setup

---
+++ client/containers/CanvasContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class CanvasContainer extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Hey Dave and Ewen!</h1>
+      </div>
+    )
+  }
+
+}
+
+export default CanvasContainer
